8db94276e593d68927e2a2f854d5d0bb019d2434
FURKAN VIJAPURA <frkgraphic@gmail.com>
2018-08-06 11:26:28 +0530

    Update Constant.swift

--- Constrants/Constant.swift
+++ Constrants/Constant.swift
@@ -1,10 +1,4 @@
-//
-//  Constant.swift
-//  DemoCheckList
-//
-//  Created by i-Phone14 on 07/10/16.
-//  Copyright © 2016 Trainee2. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
@@ -15,106 +9,7 @@ struct Constant {
     static let SCREEN_WIDTH=UIScreen.main.bounds.width
     
 
-    //MARK:- Web Service URL MACRO
-   // static let WEBSERVICE_URL="http://192.168.1.207:8080/discus-cloud-web/rs/"
-    
-//    static let WEBSERVICE_URL="http://192.168.1.193:8080/discus-cloud-web/rs/"
-    
-//    static let WEBSERVICE_URL="http://192.168.1.129:8080/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URL="http://192.168.1.129:8180/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage="http://192.168.1.129:8180/discus-cloud-web/fileservice/"
-//    static let WEBSERVICE_URLUploadImage="http://192.168.1.129:8180/discus-cloud-web/fileservice/logo/"
-    
-    //=================Arpit local==============================
-    
-//    static let WEBSERVICE_URL="http://192.168.1.129:8380/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage="http://192.168.1.129:8380/discus-cloud-web/fileservice/"
-//    static let WEBSERVICE_URLUploadImage="http://192.168.1.129:8380/discus-cloud-web/fileservice/logo/"
-    
-//    static let WEBSERVICE_URL="http://192.168.1.181:8180/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage="http://192.168.1.181:8180/discus-cloud-web/fileservice/"
-//    static let WEBSERVICE_URLUploadImage="http://192.168.1.181:8180/discus-cloud-web/fileservice/logo/"
-    
-    //==================================Test Upl Link str==============================================================
-
-//    static let WEBSERVICE_URL1="http://192.168.1.129:8380/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage1="http://192.168.1.129:8380/discus-cloud-web/fileservice/"
-//    static let WEBSERVICE_URLUploadImage1="http://192.168.1.129:8380/discus-cloud-web/fileservice/logo/"
-//
-//        static let WEBSERVICE_URL2 = "http://192.168.1.124:9090/discus-cloud-web/rs/"
-//        static let WEBSERVICE_URLGetImage2 = "http://192.168.1.124:9090/discus-cloud-web/"
-//         static let WEBSERVICE_URLUploadImage2 = "http://192.168.1.124:9090/discus-cloud-web/fileservice/logo/"
-    
-    //==================================Test Upl Link stp==============================================================
-
-    
-    //======================124 testing & AWS live beta=========================
-    
-    //-------------------------------- Beta Testing (AWS) -----------------------------//
-//    static let WEBSERVICE_URL = "http://35.162.18.26:8180/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage = "http://35.162.18.26:8180/discus-cloud-web/"
-//    static let WEBSERVICE_URLUploadImage ="http://35.162.18.26:8180/discus-cloud-web/fileservice/logo/"
-    
-//    static let WEBSERVICE_URL = "http://internal-testing.com:9090/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage = "http://internal-testing.com:9090/discus-cloud-web/"
-//    static let WEBSERVICE_URLUploadImage = "http://internal-testing.com:9090/discus-cloud-web/fileservice/logo/"
-    
-    
-//      static let WEBSERVICE_URL = "http://internal-testing.discus.solutions:8180/discus-cloud-web/rs/"
-//      static let WEBSERVICE_URLGetImage = "http://internal-testing.discus.solutions:8180/discus-cloud-web/"
-//      static let WEBSERVICE_URLUploadImage = "http://internal-testing.discus.solutions:8180/discus-cloud-web/fileservice/logo/"
-    //This is latest live productions
-     static var WEBSERVICE_URL = String()
-     static var WEBSERVICE_URLGetImage = String()
-     static var WEBSERVICE_URLUploadImage = String()
-//    static let WEBSERVICE_URL =  "http://portal-beta.discus.solutions:8280/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage = "http://portal-beta.discus.solutions:8280/discus-cloud-web/"
-//    static let WEBSERVICE_URLUploadImage = "http://portal-beta.discus.solutions:8280/discus-cloud-web/fileservice/logo/"
-    
-//        static let WEBSERVICE_URL =  "http://portal.discus.solutions/discus-cloud-web/rs/"
-//        static let WEBSERVICE_URLGetImage = "http://portal.discus.solutions/discus-cloud-web/"
-//        static let WEBSERVICE_URLUploadImage = "http://portal.discus.solutions/discus-cloud-web/fileservice/logo/"
-    //portal.discus.solutions
-    
-//    static let WEBSERVICE_URL =  "http://192.168.1.129:8380/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage = "http://192.168.1.129:8380/discus-cloud-web/"
-//    static let WEBSERVICE_URLUploadImage = "http://192.168.1.129:8380/discus-cloud-web/fileservice/logo/"
-    
-//    static let WEBSERVICE_URL =  "http://192.168.1.177:8080/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage = "http://192.168.1.177:8080/discus-cloud-web/"
-//    static let WEBSERVICE_URLUploadImage = "http://192.168.1.177:8080/discus-cloud-web/fileservice/logo/"
-    
-//    static let WEBSERVICE_URL =  "http://192.168.1.147:8180/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage = "http://192.168.1.147:8180/discus-cloud-web/"
-//    static let WEBSERVICE_URLUploadImage = "http://192.168.1.147:8180/discus-cloud-web/fileservice/logo/"
-    
-    //======================New one=========================
-
-//     static let WEBSERVICE_URL = "http://internal-testing.discus.solutions:9090/discus-cloud-web/rs/"
-//      static let WEBSERVICE_URLGetImage = "http://internal-testing.discus.solutions:9090/discus-cloud-web/"
-//     static let WEBSERVICE_URLUploadImage = "http://internal-testing.discus.solutions:9090/discus-cloud-web/fileservice/logo/"
-
-//===============================================
-//    static let WEBSERVICE_URL="http://192.168.1.147:8080/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URL="http://192.168.1.181:8080/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URL="http://192.168.1.151:8080/discus-cloud-web/rs/"
-
- //========For qualilty server=============
-    
-//    static let WEBSERVICE_URL="http://client-demo.discus.solutions/discus-cloud-web/rs/"
-//    static let WEBSERVICE_URLGetImage="http://client-demo.discus.solutions/discus-cloud-web/rs/fileservice/"
-//    static let WEBSERVICE_URLUploadImage="http://client-demo.discus.solutions/discus-cloud-web/rs/fileservice/logo/"
-
-    
-//    public static final String BASE_URL = "http://client-demo.discus.solutions/discus-cloud-web/rs/";
-//    public static final String BASE_URL_IMAGE = "http://quality-testing.discus.solutions:8080/discus-cloud-web/";
-//    public static final String BASE_URL_IMAGE_VISIT_LIST ="http://quality-testing.discus.solutions:8080/discus-cloud-web/fileservice/logo/";
-
-
-//  http://client-demo.discus.solutions/discus-cloud-web/rs/
-
-//    http://192.168.1.129:8080/discus-cloud-web/rs/validAuth/authenticateForMobile
-
+   
     
     //validAuth/authenticateForMobile
     
